827aacba25afcd04eea9c58e98b2edb9e4de1c84
MateusLucasDaSilva <mateuslukas505@gmail.com>
2025-03-01 00:21:15 -0300

    first commit

---
+++ android/app/src/main/kotlin/mateus/com/br/social_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package mateus.com.br.social_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:social_app/app/presentation/app_widget.dart';
+
+void main() {
+  runApp(const AppWidget());
+}
